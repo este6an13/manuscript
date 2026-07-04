37d0f579503f59c395e3d5489b49d80ad2bb15ea
este6an13 <diquintero@unal.edu.co>
2026-07-04 01:47:18 -0500

    add stopping condition

--- gradient_descent/main.py
+++ gradient_descent/main.py
@@ -1,14 +1,24 @@
 from linear_algebra.utils import addv, scalev
+from geometry.utils import euclidean_distance
 
-def gradient_descent(fn, params, lr, max_iters=1000):
+f = lambda params, new_params, tol : euclidean_distance(params, new_params) < tol  # noqa: E731
+g = lambda i, max_iters : i > max_iters # noqa: E731
+
+def stopping_condition(f, g):
+    if any([f, g]):
+        return True
+    return False
+
+
+def gradient_descent(fn, params, lr, max_iters=1000, tol=1e-12):
     i = 0
     while True:
-        new_params = addv(params, scalev(-lr, fn(params)))
-        params = new_params
         i += 1
-        if i >= max_iters:
-            break
-    return params
+        new_params = addv(params, scalev(-lr, fn(params)))
+        if stopping_condition(f(params, new_params, tol), g(i, max_iters)):
+            return new_params
+        # next iteration
+        params = new_params
 
 
 if __name__ == "__main__":
@@ -18,7 +28,7 @@ if __name__ == "__main__":
     params = [0.0]
     lr = 0.1
     params = gradient_descent(fn, params, lr)
-    print(params) # [2.999999999999999]
+    print(params) #  [2.9999999999963927]
 
     def fn(params):
         x = params[0]
@@ -28,5 +38,5 @@ if __name__ == "__main__":
     params = [0.0, 0.0]
     lr = 0.1
     params = gradient_descent(fn, params, lr)
-    print(params) # [2.4999999999999987, -5.4999999999999964]
+    print(params) #  [2.499999999997129, -5.499999999993069]
 
